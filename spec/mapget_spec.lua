92983136bdaff03db80f7f8fdec00290041f1ee3
Pavel Dolgov <pdolgov99@gmail.com>
2017-10-29 01:01:38 +0000

    Fix tests
     - Correct RGB range for blue (from 0 to 1)
     - Improve style

--- spec/mapget_spec.lua
+++ spec/mapget_spec.lua
@@ -11,40 +11,45 @@ describe("mapget", function()
     it("downloads tile", function()
         local mapget = assert(require 'mapget')
         local socket = assert(require 'socket.http')
-        local options = {}
-        options.kind = map
-        options.v = '4.40.1'
-        options.x = '55'
-        options.y = '79'
-        options.z = '7'
-        options.scale = '1'
-        options.lang = 'ru_RU'
-        local data = assert(mapget.mapget(socket, options))
+        local options = {
+            map_type = 'map',
+            v = '4.40.1',
+            x = '55',
+            y = '79',
+            z = '7',
+            scale = '1',
+            lang = 'ru_RU',
+        }
+        local data = assert(mapget.getTile(socket, options))
     end)
 
     it("downloads tile correctly", function()
         local mapget = assert(require 'mapget')
         local magick = assert(require 'magick')
         local socket = assert(require 'socket.http')
-        local options = {}
-        options.kind = map
-        options.v = '4.40.1'
-        options.x = '55'
-        options.y = '79'
-        options.z = '7'
-        options.scale = '1'
-        options.lang = 'ru_RU'
-        local data = assert(mapget.mapget(socket, options))
-        local output = io.open("data.png", "w")
+        local options = {
+            map_type = 'map',
+            v = '4.40.1',
+            x = '55',
+            y = '79',
+            z = '7',
+            scale = '1',
+            lang = 'ru_RU',
+        }
+        local data = assert(mapget.getTile(socket, options))
+        local output = io.open("data", "w")
         output:write(data)
         output:close()
-        local img = assert(magick.load_image("data.png"))
+        local img = assert(magick.load_image("data"))
         for x = 0, img:get_width() do
             for y = 0, img:get_height() do
                 local r, g, b, a = img:get_pixel(x, y)
-                assert (r >= 144 and r <= 184) and
-                       (g >= 170 and g <= 210) and
-                       (b >= 200 and b <= 240)
+                -- ocean: must be blue
+                assert(
+                    (r >= 0.65 and r <= 0.67) and
+                    (g >= 0.81 and g <= 0.83) and
+                    (b >= 0.90 and b <= 0.91)
+                )
             end
         end
         img:destroy()
