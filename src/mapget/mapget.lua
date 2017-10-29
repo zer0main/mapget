d9545a19483423a23ebe041bdd945987ab6c67f9
Pavel Dolgov <pdolgov99@gmail.com>
2017-10-29 03:18:44 +0000

    mapget.getTile(): add request headers

--- src/mapget/mapget.lua
+++ src/mapget/mapget.lua
@@ -4,14 +4,26 @@
 
 local mapget = {}
 
+-- TODO: check headers order!
+
+local headers_frame = {}
+headers_frame['User-Agent'] = 'Mozilla/5.0 (Windows NT 6.1; rv:52.0) Gecko/20100101 Firefox/52.0'
+headers_frame['Accept'] = '*/*'
+headers_frame['Accept-Language'] = 'en-US,en;q=0.5'
+headers_frame['Accept-Encoding'] = 'gzip, deflate, br'
+headers_frame['Referer'] = 'blablabla'
+headers_frame['Connection'] = 'keep-alive'
+
 function mapget.getTile(options)
     local http = require('socket.http')
+    local ltn12 = require('ltn12')
     local subdomain = ''
     if options.map_type == 'map' then
         subdomain = 'vec'
     else
         subdomain = 'sat'
     end
+    headers_frame['Host'] = subdomain .. '01.maps.yandex.net'
     local url_frame = "https://%s01.maps.yandex.net/tiles?l=%s&v" ..
                       "=%s&x=%s&y=%s&z=%s&scale=%s&lang=%s"
     local url = url_frame:format(
@@ -24,8 +36,13 @@ function mapget.getTile(options)
         options.scale,
         options.lang
     )
-    local data, inf, spec = http.request(url)
-    return data
+    local data = {}
+    http.request{
+        url = url,
+        sink = ltn12.sink.table(data),
+        headers = headers_frame
+    }
+    return table.concat(data)
 end
 
 return mapget
