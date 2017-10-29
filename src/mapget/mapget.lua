c66868fe39120355604ab04b4e46526965a4c91e
Pavel Dolgov <pdolgov99@gmail.com>
2017-10-29 01:48:05 +0100

    Fix mapget.getTile()
     - Pass formatted version of url to socket.request()
     - Specify correct subdomain for satellite maps
     - Improve style

--- src/mapget/mapget.lua
+++ src/mapget/mapget.lua
@@ -2,15 +2,29 @@
 -- Copyright (C) 2015 Pavel Dolgov
 -- See the LICENSE file for terms of use.
 
-local function mapget(socket, options)
-    local URL = "https://vec04.maps.yandex.net/tiles?l=%s&v" ..
-                "=%s&x=%s&y=%s&z=%s&scale=%s&lang=%s"
-    URL:format(options.kind, options.v, options.x, options.y,
-               options.z, options.scale, options.lang)
-    local data, inf, spec = socket.request(URL)
+local mapget = {}
+
+function mapget.getTile(socket, options)
+    local subdomain = ''
+    if options.map_type == 'map' then
+        subdomain = 'vec'
+    else
+        subdomain = 'sat'
+    end
+    local url_frame = "https://%s01.maps.yandex.net/tiles?l=%s&v" ..
+                      "=%s&x=%s&y=%s&z=%s&scale=%s&lang=%s"
+    local url = url_frame:format(
+        subdomain,
+        options.map_type,
+        options.v,
+        options.x,
+        options.y,
+        options.z,
+        options.scale,
+        options.lang
+    )
+    local data, inf, spec = socket.request(url)
     return data
 end
 
-return {
-    mapget = 'mapget',
-}
+return mapget
